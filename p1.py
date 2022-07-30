21108c0058887c21f2557a83f45edfe8d35c5296
Alejandroa24 <11-10108@usb.ve>
2022-07-30 02:38:36 -0400

    se agrego el branch del inventario, se creo el inventario en un archivo csv y se creo un archivo gitignore para no subirlo al repositorio se explico todo en el README

--- p1.py
+++ p1.py
@@ -4,16 +4,77 @@ import meraki
 import requests
 dashboard = meraki.DashboardAPI(API_KEY)
 response = dashboard.organizations.getOrganizations() #lista detallada de organizaciones
-#print(response)
+print(response)
 
 listica=[]
 file= open("listaOrg.txt", "w")
 file.write("Lista de nombre de organizaciones\n")
+dispositivos=open("dispositivosWirelessYApplianceDetallado.csv","w")
+dispositivos.write("INVENTARIO DE DISPOSITIVOS WIRELESS Y APPLIANCE \n Alejandro Blanco 11-10108\n")
 
 for i in response:
     listica.append(i["name"])
     file.write("\n")
     file.write(i["name"])
   
-print(listica)
 file.close()
+
+inventario= dashboard.organizations.getOrganizationDevices("681155")
+
+for k in inventario:
+    if k["productType"]=="wireless" or k["productType"]=="appliance":
+        tipoProducto=k["productType"]
+        nombreEquipo=k["name"]
+        modeloEquipo=k["model"]
+        mac=k["mac"]
+        ipPublica=k["networkId"]
+        numSerial=k["serial"]
+        statusDisp=k["configurationUpdatedAt"]
+        
+        print("\n\nTipo del producto : ",tipoProducto)
+        print("Nombre del equipo: ",nombreEquipo)
+        print("Modelo: ", modeloEquipo)
+        print("Mac: ", mac)
+        print("Ip publica: ", ipPublica)
+        print("Serial: " ,numSerial)
+        print("Status del dispositivo: " ,statusDisp+"\n")
+        
+        dispositivos.write("\n")
+        dispositivos.write("Tipo del producto : ")
+        dispositivos.write(tipoProducto)
+        dispositivos.write("\n")
+        dispositivos.write("Nombre del equipo: ")
+        dispositivos.write(nombreEquipo)
+        dispositivos.write("\n")
+        dispositivos.write("Modelo: ")
+        dispositivos.write(modeloEquipo)
+        dispositivos.write("\n")
+        dispositivos.write("Mac: ")
+        dispositivos.write(mac)
+        dispositivos.write("\n")
+        dispositivos.write("Ip publica: ")
+        dispositivos.write(ipPublica)
+        dispositivos.write("\n")
+        dispositivos.write("Serial: ")
+        dispositivos.write(numSerial)
+        dispositivos.write("\n")
+        dispositivos.write("Status del dispositivo: ")
+        dispositivos.write(statusDisp)
+        dispositivos.write("\n")
+
+        if k["productType"]=="wireless":
+            lanIp5=k["lanIp"]
+            if not lanIp5:
+                lanIp5="N/A"
+            dispositivos.write("Direccion Lan: ")
+            dispositivos.write(lanIp5)
+            dispositivos.write("\n")
+        if k["productType"]=="appliance":
+            wan1Ip=k["wan1Ip"]
+            if not wan1Ip:
+                wan1Ip="N/A"
+
+            dispositivos.write("Direccion wan1: ")
+            dispositivos.write(wan1Ip)
+            dispositivos.write("\n")
+dispositivos.close()
